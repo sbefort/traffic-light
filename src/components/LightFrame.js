4670cf9c9ebf31b7e9b10fa0fa79e30f95ea0742
Shaun Befort <shaun.befort@gomolearning.com>
2022-01-16 13:25:57 -0600

    Fixed bug with pointer cursor in wrong state

--- src/components/LightFrame.js
+++ src/components/LightFrame.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LightFrame = ({ children, isActive, onClick, className = '' }) => {
   const classNames = [className];
-  if (isActive) classNames.push('cursor-pointer')
+  if (!isActive) classNames.push('cursor-pointer')
   return (
     <div aria-label="traffic light" onClick={onClick} className={classNames.join(' ')}>
       { children }
